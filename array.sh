5a7b3b1d473496b9433ea7c8c9d5d47ed00595f4
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-07-11 19:15:14 +0900

    create array.sh

---
+++ array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ARRAY=(item1 item2 item3 item4)
+ARRAY[0]="ITEM1"
+ARRAY[2]="ITEM3"
+
+echo "ARRAY[0]: ${ARRAY[0]}"
+echo "ARRAY[1]: ${ARRAY[1]}"
+
+#全てのアイテムをアクセスする
+echo "ARRAY[*]: ${ARRAY[*]}"
+echo "ARRAY[@]: ${ARRAY[@]}"
